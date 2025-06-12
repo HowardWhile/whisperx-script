54437d1f7e1b7c0d3ddf46464d66032863c4bc09
Howard <makubex49@gmail.com>
2025-06-12 16:15:08 +0800

    我的環境換成 miniconda 改成 run_app.sh改成通用的寫法

--- service_script/run_app.sh
+++ service_script/run_app.sh
@@ -3,7 +3,15 @@
 echo "🚀 啟動 WhisperX WebUI..."
 
 # 初始化 Conda 環境
-source ~/anaconda3/etc/profile.d/conda.sh
+if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+    source "$HOME/miniconda3/etc/profile.d/conda.sh"
+elif [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+    source "$HOME/anaconda3/etc/profile.d/conda.sh"
+else
+    echo "❌ 找不到 conda.sh，請確認是否已安裝 Conda。"
+    exit 1
+fi
+
 source ~/.bashrc
 
 # 切換到目錄並啟動
